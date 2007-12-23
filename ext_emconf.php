045f8229058d04c4c8c5925dc1b1b15b562e8ffa
Ingo Renner <ingo@typo3.org>
2007-12-23 23:41:07 +0000

    TER version bumped to 2.2.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tt_address/trunk@7668 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tt_address"
 #
-# Auto generated 23-12-2007 17:34
+# Auto generated 24-12-2007 00:40
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'ingo-renner.com',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '2.2.0',
-	'_md5_values_when_last_written' => 'a:34:{s:9:"ChangeLog";s:4:"de4e";s:4:"ToDo";s:4:"0e3c";s:9:"build.xml";s:4:"d7c6";s:20:"class.ext_update.php";s:4:"42a2";s:37:"class.tx_ttaddress_addfieldstosel.php";s:4:"b849";s:36:"class.tx_ttaddress_addfilestosel.php";s:4:"9fb3";s:29:"class.tx_ttaddress_compat.php";s:4:"3d2c";s:45:"class.tx_ttaddress_tcefunc_selecttreeview.php";s:4:"0b4c";s:31:"class.tx_ttaddress_treeview.php";s:4:"ca8f";s:21:"ext_conf_template.txt";s:4:"a7cf";s:12:"ext_icon.gif";s:4:"e1bc";s:15:"ext_icon__h.gif";s:4:"1bdd";s:15:"ext_icon__x.gif";s:4:"aec5";s:17:"ext_localconf.php";s:4:"d215";s:14:"ext_tables.php";s:4:"a6a3";s:14:"ext_tables.sql";s:4:"52b4";s:25:"icon_tt_address_group.gif";s:4:"156c";s:13:"locallang.xml";s:4:"d478";s:27:"locallang_csh_ttaddress.xml";s:4:"1fd9";s:17:"locallang_tca.xml";s:4:"3cf8";s:7:"tca.php";s:4:"3cd8";s:14:"doc/manual.sxw";s:4:"04a4";s:14:"pi1/ce_wiz.gif";s:4:"2648";s:30:"pi1/class.tx_ttaddress_pi1.php";s:4:"1479";s:38:"pi1/class.tx_ttaddress_pi1_wizicon.php";s:4:"919c";s:16:"pi1/flexform.xml";s:4:"c87b";s:17:"pi1/locallang.xml";s:4:"222a";s:20:"pi1/locallang_ff.xml";s:4:"3a81";s:21:"res/default_hcard.gif";s:4:"25a0";s:21:"res/default_hcard.htm";s:4:"463d";s:24:"static/old/constants.txt";s:4:"9eaf";s:20:"static/old/setup.txt";s:4:"c944";s:24:"static/pi1/constants.txt";s:4:"d41d";s:20:"static/pi1/setup.txt";s:4:"f80d";}',
+	'version' => '2.2.1',
+	'_md5_values_when_last_written' => 'a:34:{s:9:"ChangeLog";s:4:"024b";s:4:"ToDo";s:4:"9983";s:9:"build.xml";s:4:"d7c6";s:20:"class.ext_update.php";s:4:"42a2";s:37:"class.tx_ttaddress_addfieldstosel.php";s:4:"b849";s:36:"class.tx_ttaddress_addfilestosel.php";s:4:"9fb3";s:29:"class.tx_ttaddress_compat.php";s:4:"3d2c";s:45:"class.tx_ttaddress_tcefunc_selecttreeview.php";s:4:"0b4c";s:31:"class.tx_ttaddress_treeview.php";s:4:"ca8f";s:21:"ext_conf_template.txt";s:4:"a7cf";s:12:"ext_icon.gif";s:4:"e1bc";s:15:"ext_icon__h.gif";s:4:"1bdd";s:15:"ext_icon__x.gif";s:4:"aec5";s:17:"ext_localconf.php";s:4:"d215";s:14:"ext_tables.php";s:4:"a6a3";s:14:"ext_tables.sql";s:4:"52b4";s:25:"icon_tt_address_group.gif";s:4:"156c";s:13:"locallang.xml";s:4:"d478";s:27:"locallang_csh_ttaddress.xml";s:4:"1fd9";s:17:"locallang_tca.xml";s:4:"3cf8";s:7:"tca.php";s:4:"3cd8";s:14:"doc/manual.sxw";s:4:"2510";s:14:"pi1/ce_wiz.gif";s:4:"2648";s:30:"pi1/class.tx_ttaddress_pi1.php";s:4:"bc93";s:38:"pi1/class.tx_ttaddress_pi1_wizicon.php";s:4:"919c";s:16:"pi1/flexform.xml";s:4:"c87b";s:17:"pi1/locallang.xml";s:4:"222a";s:20:"pi1/locallang_ff.xml";s:4:"3a81";s:21:"res/default_hcard.gif";s:4:"25a0";s:21:"res/default_hcard.htm";s:4:"463d";s:24:"static/old/constants.txt";s:4:"9eaf";s:20:"static/old/setup.txt";s:4:"c944";s:24:"static/pi1/constants.txt";s:4:"d41d";s:20:"static/pi1/setup.txt";s:4:"f80d";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',

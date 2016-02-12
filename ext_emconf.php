c2a30edb5af1e71ab2782e82ab867b85b902cb8a
Benni Mack <benni@typo3.org>
2016-02-12 10:32:00 +0100

    [FOLLOWUP][TASK] Clean up files and versions
    Fixed a problem with the version constraint while uploading.
    
    Change-Id: Iee66c951dab9163a3b89f1e73e93612881b23aee
    Releases: master
    Resolves: #72560

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '3.0.0',
     'constraints' => array(
         'depends' => array(
-            'frontend' => '6.2.2-7.9.99'
+            'typo3' => '6.2.2-7.9.99'
         ),
         'conflicts' => array(
         ),

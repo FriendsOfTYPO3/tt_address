370ccac19acd08eb2887bb0307201a7d0636383b
Benni Mack <benni@typo3.org>
2016-04-18 07:07:49 +0200

    [TASK] Raise version to 3.0.1 and update compat for TYPO3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,10 +20,10 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 1,
     'author' => 'tt_address Development Team',
     'author_email' => 'friendsof@typo3.org',
-    'version' => '3.0.0',
+    'version' => '3.0.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.2-7.9.99'
+            'typo3' => '6.2.2-8.9.99'
         ),
         'conflicts' => array(
         ),

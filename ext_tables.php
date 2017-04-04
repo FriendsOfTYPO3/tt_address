cd8de7ed2e490a9154018e5e7ea1deb5d52a7a85
Clemens Riccabona <clemens@riccabona.it>
2017-04-04 16:31:28 +0200

    [TASK] Move locallang_csh.xml to XLIFF in Resources/Private/Language/

--- ext_tables.php
+++ ext_tables.php
@@ -3,3 +3,4 @@ defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tt_address');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tt_address');
+

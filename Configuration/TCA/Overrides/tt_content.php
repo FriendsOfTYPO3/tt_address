3e2d4530720e727c19d50458aac525df260aef5e
Benni Mack <benni@typo3.org>
2017-08-09 14:49:31 +0200

    [TASK] Moved FlexForm to Configuration/FlexForms

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,4 +13,4 @@ defined('TYPO3_MODE') or die();
 // add flexform to pi1
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['tt_address_pi1'] = 'pi_flexform';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['tt_address_pi1'] = 'layout,select_key,pages,recursive';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('tt_address_pi1', 'FILE:EXT:tt_address/pi1/flexform.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('tt_address_pi1', 'FILE:EXT:tt_address/Configuration/FlexForms/Pi1.xml');

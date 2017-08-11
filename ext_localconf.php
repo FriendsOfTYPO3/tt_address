e39a4409b1548407a724d40276988900ed9ae60a
Benni Mack <benni@typo3.org>
2017-08-11 22:02:21 +0200

    [CLEANUP] Use proper PHP variables and class names in ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,21 +7,21 @@ defined('TYPO3_MODE') or die();
 ');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
-    $_EXTKEY,
+    'tt_address',
     'pi1/class.tx_ttaddress_pi1.php',
     '_pi1',
     'list_type',
-    1
+    true
 );
 
 if (TYPO3_MODE === 'BE') {
     $settings = \TYPO3\TtAddress\Utility\SettingsUtility::getSettings();
     if ($settings->isStoreBackwardsCompatName()) {
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
-            'TYPO3\\TtAddress\\Hooks\\DataHandler\\BackwardsCompatibilityNameFormat';
+            \TYPO3\TtAddress\Hooks\DataHandler\BackwardsCompatibilityNameFormat::class;
     }
 }
 
 // Update scripts
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tt_address_group'] = 'TYPO3\\TtAddress\\Updates\\AddressGroupToSysCategory';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tt_address_image'] = 'TYPO3\\TtAddress\\Updates\\ImageToFileReference';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tt_address_group'] = \TYPO3\TtAddress\Updates\AddressGroupToSysCategory::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tt_address_image'] = \TYPO3\TtAddress\Updates\ImageToFileReference::class;

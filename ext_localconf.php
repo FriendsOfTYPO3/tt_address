231b8e6506e9fa56bb4dae445047580531a838d4
Benni Mack <benni@typo3.org>
2017-08-22 08:24:28 +0200

    [TASK] Remove superfluous check for settings on hook registration
    This is done within the hook itself as well and makes ext_localconf context-free.

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,13 +11,7 @@ defined('TYPO3_MODE') or die();
     true
 );
 
-if (TYPO3_MODE === 'BE') {
-    $settings = \TYPO3\TtAddress\Utility\SettingsUtility::getSettings();
-    if ($settings->isStoreBackwardsCompatName()) {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] =
-            \TYPO3\TtAddress\Hooks\DataHandler\BackwardsCompatibilityNameFormat::class;
-    }
-}
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \TYPO3\TtAddress\Hooks\DataHandler\BackwardsCompatibilityNameFormat::class;
 
 // Update scripts
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tt_address_group'] = \TYPO3\TtAddress\Updates\AddressGroupToSysCategory::class;

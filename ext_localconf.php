d0daf441b98608fa14963b1c816ae0f21572675a
Clemens Riccabona <clemens@riccabona.it>
2018-06-28 19:48:37 +0200

    [FOLLOWUP][TASK] Merging master with fork - wizicon part of the story

--- ext_localconf.php
+++ ext_localconf.php
@@ -65,17 +65,19 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tt_address_l
     ['source' => 'EXT:tt_address/Resources/Public/Icons/ContentElementWizard.gif']
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-    mod.wizards.newContentElement.wizardItems.plugins {
-        elements.tx_ttaddress_pi1 {
-            iconIdentifier = tt-address-plugin
-            title = LLL:EXT:tt_address/Resources/Private/Language/locallang_pi1.xlf:pi1_title
-            description = LLL:EXT:tt_address/Resources/Private/Language/locallang_pi1.xlf:pi1_plus_wiz_description
-            tt_content_defValues {
-                CType = list
-                list_type = tt_address_pi1
+if ($localExtConf['activatePiBase'] === 1) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+        mod.wizards.newContentElement.wizardItems.plugins {
+            elements.tx_ttaddress_pi1 {
+                iconIdentifier = tt-address-plugin
+                title = LLL:EXT:tt_address/Resources/Private/Language/locallang_pi1.xlf:pi1_title
+                description = LLL:EXT:tt_address/Resources/Private/Language/locallang_pi1.xlf:pi1_plus_wiz_description
+                tt_content_defValues {
+                    CType = list
+                    list_type = tt_address_pi1
+                }
             }
+            show :=addToList(tx_ttaddress_pi1)
         }
-        show :=addToList(tx_ttaddress_pi1)
-    }
-');
+    ');
+}

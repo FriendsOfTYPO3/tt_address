0ed1eca6209f1649a2a84b42f81f64e95a01ef1a
Torben Hansen <derhansen@gmail.com>
2019-02-08 19:18:53 +0100

    [FEATURE] Added allowLanguageSynchronization to TCA (#162)
    This commit adds "allowLanguageSynchronization" to nearly all TCA
    fields, so user can decide, if value of default language should be
    used or not.
    
    Excepted fields: fender, first_name, middle_name, last_name
    
    Refs. #160

---
+++ Configuration/TCA/Overrides/tt_address.php
@@ -0,0 +1,5 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+// Enable language synchronisation for the category field
+$GLOBALS['TCA']['tt_address']['columns']['categories']['config']['behaviour']['allowLanguageSynchronization'] = true;

d7f408638b5d288a776d9e756a4fec09548ef05c
Georg Ringer <georg.ringer@gmail.com>
2019-03-07 09:52:02 +0100

    [TASK] Add static google maps

---
+++ Classes/ViewHelpers/GoogleMapsStaticViewHelper.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace FriendsOfTYPO3\TtAddress\ViewHelpers;
+
+/**
+ * This file is part of the "tt_address" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+use FriendsOfTYPO3\TtAddress\Domain\Model\Address;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+class GoogleMapsStaticViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    /**
+     * Initialize arguments
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('addresses', 'mixed', 'Addresses', true);
+        $this->registerArgument('parameters', 'array', 'Parameters', true);
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    )
+    {
+        $mapArguments = $arguments['parameters'];
+
+        $markers = [];
+        foreach ($arguments['addresses'] as $address) {
+            /** @var Address $address */
+            $markers[] = '&markers=' . $address->getLatitude() . ',' . $address->getLongitude();
+        }
+        if (count($markers) === 1) {
+            $mapArguments['zoom'] = 13;
+        }
+
+        return 'http://maps.googleapis.com/maps/api/staticmap?' . GeneralUtility::implodeArrayForUrl('', $mapArguments, '', true) . implode('', $markers);
+    }
+}

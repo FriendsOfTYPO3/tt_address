caeb455d5168915e860e558692074b5b8333444b
Georg Ringer <georg.ringer@gmail.com>
2019-08-26 22:35:31 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- Classes/Domain/Model/Dto/Settings.php
+++ Classes/Domain/Model/Dto/Settings.php
@@ -8,7 +8,6 @@ namespace FriendsOfTYPO3\TtAddress\Domain\Model\Dto;
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  */
-
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;

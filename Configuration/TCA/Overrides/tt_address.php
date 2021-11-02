8ced7e50588d1c334a3d4e0b3304777cbc764c27
Georg Ringer <georg.ringer@gmail.com>
2021-11-02 11:49:32 +0100

    [TASK] Use TCA type language in TYPO3 11

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -1,5 +1,14 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-// Enable language synchronisation for the category field
-$GLOBALS['TCA']['tt_address']['columns']['categories']['config']['behaviour']['allowLanguageSynchronization'] = true;
+call_user_func(static function () {
+    // Enable language synchronisation for the category field
+    $GLOBALS['TCA']['tt_address']['columns']['categories']['config']['behaviour']['allowLanguageSynchronization'] = true;
+
+    $versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+    if ($versionInformation->getMajorVersion() === 11) {
+        $GLOBALS['TCA']['tt_address']['columns']['sys_language_uid']['config'] = [
+            'type' => 'language'
+        ];
+    }
+});

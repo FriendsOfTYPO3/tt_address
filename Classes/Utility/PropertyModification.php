8ed29bb9c4807503c138f89c1d606b05fbda9cf8
Georg Ringer <georg.ringer@gmail.com>
2022-01-14 21:25:05 +0100

    [FEATURE] Extract property modification to utility
    Add ViewHelpers to use same logic as in model to modify phone number and domain
    Resolves: #389

---
+++ Classes/Utility/PropertyModification.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace FriendsOfTYPO3\TtAddress\Utility;
+
+/**
+ * This file is part of the "tt_address" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+/**
+ * Modify properties of the address model
+ */
+class PropertyModification
+{
+
+    /**
+     * Get cleaned number of a given telephone, fax or mobile number.
+     * It removes all chars which are not possible to enter on your cell phone.
+     *
+     * @param string $number
+     * @return string
+     */
+    public static function getCleanedNumber(string $number): string
+    {
+        $number = trim($number);
+
+        // Remove 0 on +49(0)221, but keep 0 on (0)221
+        if (strpos($number, '(0)') > 0) {
+            $number = str_replace('(0)', '', $number);
+        }
+
+        return preg_replace('/[^0-9#+*]/', '', $number);
+    }
+
+    public static function getCleanedDomain(string $domain): string
+    {
+        $domain = trim($domain);
+        if (!$domain) {
+            return '';
+        }
+        $parts = str_replace(['\\\\', '\\"'], ['\\', '"'], str_getcsv($domain, ' '));
+        return $parts[0];
+    }
+}
---
+++ Classes/ViewHelpers/Clean/DomainViewHelper.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace FriendsOfTYPO3\TtAddress\ViewHelpers\Clean;
+
+/**
+ * This file is part of the "tt_address" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+use FriendsOfTYPO3\TtAddress\Utility\PropertyModification;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+class DomainViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    /**
+     * Initialize arguments
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('value', 'string', 'value');
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $value = $arguments['value'] ?: $renderChildrenClosure();
+        return PropertyModification::getCleanedDomain($value);
+    }
+}

5a592234d528a6043a91ec0fad1116efcd2c1e6f
Uwe <trhawkeye09@gmail.com>
2023-01-19 11:28:46 +0100

    Fix check for sortBy field
    See comment here: https://github.com/FriendsOfTYPO3/tt_address/issues/421#issuecomment-13966325404

--- Classes/Utility/TypoScript.php
+++ Classes/Utility/TypoScript.php
@@ -39,7 +39,7 @@ class TypoScript
                     }
                 }
             } else {
-                if ($fieldName === 'sortBy' && ($previousData['sortBy'] ?? '' === 'default') && (($tsData['settings']['sortBy'] ?? '') !== '')) {
+                if ($fieldName === 'sortBy' && (($previousData['sortBy'] ?? '') === 'default') && (($tsData['settings']['sortBy'] ?? '') !== '')) {
                     unset($previousData['sortBy']);
                 }
                 // if flexform setting is empty and value is available in TS

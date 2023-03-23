593e47304b830d3fca28388b216ef37009d5cfb0
mtness <markus@timtner.com>
2023-03-23 13:59:25 +0100

    abclist

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -22,4 +22,11 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSi
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:tt_address/Configuration/FlexForms/List.xml');
 
+$abcpluginSignature = 'ttaddress_abclistview';
+
+$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$abcpluginSignature] = \FriendsOfTYPO3\TtAddress\FormEngine\TtAddressPreviewRenderer::class;
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$abcpluginSignature] = 'select_key,pages,recursive';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$abcpluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($abcpluginSignature, 'FILE:EXT:tt_address/Configuration/FlexForms/List.xml');
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tt_address');

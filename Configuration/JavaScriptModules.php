a05d4b88ae00f0ef80975eac1d6107e67e164bd3
Oliver Hader <oliver@typo3.org>
2023-05-11 22:13:39 +0200

    [TASK] Remove core dependency for JavaScript modules

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -2,7 +2,6 @@
 defined('TYPO3') or die;
 
 return [
-    'dependencies' => ['core'],
     'imports' => [
         '@friendsoftypo3/tt-address/' => 'EXT:tt_address/Resources/Public/JavaScript/esm/',
     ],

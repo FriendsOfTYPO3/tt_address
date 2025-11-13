8a77a0b857f7da417c7336f54bc8a730200724a8
Georg Ringer <georg.ringer@gmail.com>
2025-11-13 21:15:26 +0100

    [TASK] Update plugin registration

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,7 +2,7 @@
 
 defined('TYPO3') or die;
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+$pluginSignature = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'tt_address',
     'ListView',
     'LLL:EXT:tt_address/Resources/Private/Language/db/locallang.xlf:extbase_title',
@@ -11,11 +11,14 @@ defined('TYPO3') or die;
     'LLL:EXT:tt_address/Resources/Private/Language/db/locallang.xlf:extbase_description'
 );
 
-$pluginSignature = 'ttaddress_listview';
-
 $GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = \FriendsOfTYPO3\TtAddress\FormEngine\TtAddressPreviewRenderer::class;
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key,pages,recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:tt_address/Configuration/FlexForms/List.xml');
-
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('*', 'FILE:EXT:tt_address/Configuration/FlexForms/List.xml', $pluginSignature);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin, pi_flexform',
+    $pluginSignature,
+    'after:palette:headers'
+);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tt_address');

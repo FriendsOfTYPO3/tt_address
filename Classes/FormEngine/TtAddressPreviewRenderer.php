6922db9103df393877f9472f0838352efcbf3dc5
Georg Ringer <georg.ringer@gmail.com>
2025-11-14 09:06:19 +0100

    [TASK] Fix preview in backend

--- Classes/FormEngine/TtAddressPreviewRenderer.php
+++ Classes/FormEngine/TtAddressPreviewRenderer.php
@@ -14,11 +14,16 @@ namespace FriendsOfTYPO3\TtAddress\FormEngine;
 use Doctrine\DBAL\ArrayParameterType;
 use TYPO3\CMS\Backend\Preview\StandardContentPreviewRenderer;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Backend\View\Event\PageContentPreviewRenderingEvent;
+use TYPO3\CMS\Backend\View\BackendLayout\Grid\GridColumnItem;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
+use TYPO3\CMS\Core\Domain\RecordInterface;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Service\FlexFormService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\View\ViewFactoryData;
+use TYPO3\CMS\Core\View\ViewFactoryInterface;
+use TYPO3\CMS\Fluid\View\StandaloneView;
 
 /**
  * Improve the rendering of the plugin in page module
@@ -44,20 +49,32 @@ class TtAddressPreviewRenderer extends StandardContentPreviewRenderer
         ],
     ];
 
-    public function __invoke(PageContentPreviewRenderingEvent $event): void
+    public function renderPageModulePreviewContent(GridColumnItem $item): string
     {
-        $row = $event->getRecord();
-        if ($row['list_type'] !== 'ttaddress_listview') {
-            return;
+        if (class_exists(StandaloneView::class)) {
+            $view = GeneralUtility::makeInstance(StandaloneView::class);
+            $view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName('EXT:tt_address/Resources/Private/Templates/Backend/PluginPreview.html'));
+        } else {
+            $viewFactoryData = new ViewFactoryData(
+                templatePathAndFilename: 'EXT:tt_address/Resources/Private/Templates/Backend/PluginPreview.html'
+            );
+            $view = GeneralUtility::makeInstance(ViewFactoryInterface::class)->create($viewFactoryData);
         }
-        $row = $this->enrichRow($row);
-        $event->setRecord($row);
+        $row = $item->getRecord();
+        if ((new Typo3Version())->getMajorVersion() >= 14) {
+            /** @var RecordInterface  $row */
+            $row = $row->getRawRecord()->toArray();
+        }
+
+        $view->assignMultiple($this->enrichRow($row));
+
+        return $view->render();
     }
 
     protected function enrichRow(array $row): array
     {
         $settings = $this->getFlexFormData($row['pi_flexform'] ?? '');
-
+        $row['pi_flexform_transformed'] = $settings;
         foreach ($this->recordMapping as $fieldName => $fieldConfiguration) {
             if ($settings['settings'][$fieldName] ?? false) {
                 $records = $this->getRecords($fieldConfiguration['table'], $settings['settings'][$fieldName]);
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -11,7 +11,8 @@ $pluginSignature = \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'LLL:EXT:tt_address/Resources/Private/Language/db/locallang.xlf:extbase_description'
 );
 
-$GLOBALS['TCA']['tt_content']['types']['list']['previewRenderer'][$pluginSignature] = \FriendsOfTYPO3\TtAddress\FormEngine\TtAddressPreviewRenderer::class;
+$GLOBALS['TCA']['tt_content']['types'][$pluginSignature]['previewRenderer'] = \FriendsOfTYPO3\TtAddress\FormEngine\TtAddressPreviewRenderer::class;
+
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key,pages,recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('*', 'FILE:EXT:tt_address/Configuration/FlexForms/List.xml', $pluginSignature);
